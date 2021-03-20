59422b6e201bf46dfb18fd2ca0979f7245a99ab7
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-20 15:45:23 -0300

    Created the file config.php and modified index.php

---
+++ config.php
@@ -0,0 +1,10 @@
+﻿<?php
+$dsn =  "mysql:dbname=blog;host=127.0.0.1";
+$dbuser = "root";
+$dbpass = "";
+
+try {
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+} catch (PDOException $e) {
+    echo "Falhou a conexão: "  .$e->getMessage();
+}
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <table border="1" width="100%">
     <th>Nome</th>
     <th>E-mail</th>
